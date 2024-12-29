7376c9d2742e756ed08addee1fe37a68137d2194
Yifei Cheng <ycheng517@gmail.com>
2024-12-29 18:37:27 -0500

    make things work with jazzy

--- easy_handeye2/launch/calibrate.launch.py
+++ easy_handeye2/launch/calibrate.launch.py
@@ -1,7 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import LaunchConfigurationEquals
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration, EqualsSubstitution
 from launch_ros.actions import Node
 
 from easy_handeye2.common_launch import arg_calibration_type, arg_tracking_base_frame, arg_tracking_marker_frame, arg_robot_base_frame, \
@@ -11,13 +10,14 @@ from easy_handeye2.common_launch import arg_calibration_type, arg_tracking_base_
 def generate_launch_description():
     arg_name = DeclareLaunchArgument('name')
 
+    calibration_type = LaunchConfiguration('calibration_type')
     node_dummy_calib_eih = Node(package='tf2_ros', executable='static_transform_publisher', name='dummy_publisher',
-                                condition=LaunchConfigurationEquals('calibration_type', 'eye_in_hand'),
+                                condition=EqualsSubstitution(calibration_type, 'eye_in_hand'),
                                 arguments=f'--x 0 --y 0 --z 0.1 --qx 0 --qy 0 --qz 0 --qw 1'.split(' ') + ['--frame-id', LaunchConfiguration('robot_effector_frame'),
                                                                                                            '--child-frame-id', LaunchConfiguration('tracking_base_frame')])
 
     node_dummy_calib_eob = Node(package='tf2_ros', executable='static_transform_publisher', name='dummy_publisher',
-                                condition=LaunchConfigurationEquals('calibration_type', 'eye_on_base'),
+                                condition=EqualsSubstitution(calibration_type, 'eye_on_base'),
                                 arguments=f'--x 1 --y 0 --z 0 --qx 0 --qy 0 --qz 0 --qw 1'.split(' ') + ['--frame-id', LaunchConfiguration('robot_base_frame'),
                                                                                                          '--child-frame-id', LaunchConfiguration('tracking_base_frame')])
 
